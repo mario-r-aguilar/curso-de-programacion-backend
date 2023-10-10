d1a83ce023824d13274e51067fb3f54802862040
Mario Aguilar <mariocba150@gmail.com>
2023-10-10 02:41:50 -0300

    Se agregan comentarios. Se finaliza el entregable 03

--- Entregable_03/src/app.js
+++ Entregable_03/src/app.js
@@ -3,16 +3,19 @@ import { productManager } from './ProductManager.js';
 
 const app = express();
 
+// Para el manejo de datos complejos
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/products', async (req, res) => {
 	try {
 		let limit = req.query.limit;
 		let productList = await productManager.getProducts();
+
 		if (limit) {
 			const productsLimited = productList.slice(0, limit);
 			return res.send({ productsLimited });
 		}
+
 		return res.send({ productList });
 	} catch (error) {
 		return res.send(`Error interno del servidor: ${error}`);
@@ -22,9 +25,9 @@ app.get('/products', async (req, res) => {
 app.get('/products/:pid', async (req, res) => {
 	try {
 		let { pid } = req.params;
-		pid = parseInt(pid);
-
+		pid = parseInt(pid); // Convierte el id a tipo número, tal cual está almacenado en el archivo json
 		const product = await productManager.getProductById(pid);
+
 		if (product) {
 			return res.send(product);
 		} else {
