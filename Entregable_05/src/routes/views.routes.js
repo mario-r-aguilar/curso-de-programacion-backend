a6c70cf2be0f734adf37dfc5ee22d7c138ff7f04
Mario Aguilar <mariocba150@gmail.com>
2023-10-28 21:04:14 -0300

    mejoro logica de views.routes.js y front del home.handlebars. Comienzo a trabajar con realTimeProducts.hsndlebars

--- Entregable_05/src/routes/views.routes.js
+++ Entregable_05/src/routes/views.routes.js
@@ -4,14 +4,18 @@ import { productManager } from '../ProductManager.js';
 const viewsRouter = Router();
 
 viewsRouter.get('/', async (req, res) => {
-	const productsList = await productManager.getProducts();
-	const isProductsList = productsList ? true : false;
+	try {
+		const productsList = await productManager.getProducts();
+		let isProductsList = Array.isArray(productsList);
 
-	res.render('home', {
-		isProductsList,
-		productsList,
-		title: 'Lista de productos disponibles',
-	});
+		res.render('home', {
+			isProductsList,
+			productsList,
+			title: 'Lista de productos disponibles',
+		});
+	} catch (error) {
+		return res.status(500).send(`Error interno del servidor: ${error}`);
+	}
 });
 
 viewsRouter.get('/realtimeproducts', (req, res) => {
